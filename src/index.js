f9f1eeea4719cf40ffc4ea72d80217a1e1b065c1
FRSK <105488329+FlorianRampazzi@users.noreply.github.com>
2022-11-27 18:35:43 +0100

    Revert "Nav link"

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from './App'
-import './styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
-
+  </React.StrictMode>
 );
 
-
-
-// export default function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <Header />
-//         <Routes>
-//           <Route path="/about">
-//             <About />
-//           </Route>
-//           {/* <Route path="/product">
-//             <Product />
-//           </Route> */}
-//           <Route exact path="/">
-//             <Home />
-//           </Route>
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
