c3fdc991026380038cb9e41b8d756e368df1011c
Florian Rampazzi <florian.rampazzi@gmail.com>
2022-11-28 01:44:56 +0100

    Redirect unkown accomodation ids to NotFound page

--- src/pages/Accomodation/Accomodation.js
+++ src/pages/Accomodation/Accomodation.js
@@ -1,35 +1,42 @@
-import { useParams } from "react-router-dom"
+import { useParams, Navigate } from 'react-router-dom'
 import accomodationList from '../../datas/accomodationList.json'
-import Collapse from "../../components/Collapse/Collapse"
+import Collapse from '../../components/Collapse/Collapse'
 import Rating from '../../components/Rating/Rating'
 import Tag from '../../components/Tag/Tag'
-import Slideshow from "../../components/Slideshow/Slideshow"
-import Host from "../../components/Host/Host"
+import Slideshow from '../../components/Slideshow/Slideshow'
+import Host from '../../components/Host/Host'
 import './Accomodation.css'
 
 export default function Accomodation() {
   const params = useParams()
-  const accomodation = accomodationList.filter((objectJson) => objectJson.id === params.id)[0]
-  
+
+  let accomodation = accomodationList.filter(
+    (objectJson) => objectJson.id === params.id
+  )
+
+  accomodation = accomodation.length !== 0 ? accomodation[0] : false
+  if (accomodation === false) {
+    return <Navigate to="/notfound" replace />
+  }
+
   return (
-    <div className='kasa-accomodation'>
-      <div className='kasa-accomodation-slideshow'>
+    <div className="kasa-accomodation">
+      <div className="kasa-accomodation-slideshow">
         <Slideshow slides={accomodation.pictures} alt={accomodation.title} />
       </div>
-      <h1 className='kasa-accomodation-title'>{accomodation.title}</h1>
-      <p className='kasa-accomodation-location'>{accomodation.location}</p>
-      <div className='kasa-accomodation-tags'>
+      <h1 className="kasa-accomodation-title">{accomodation.title}</h1>
+      <p className="kasa-accomodation-location">{accomodation.location}</p>
+      <div className="kasa-accomodation-tags">
         <Tag content={accomodation.tags} />
       </div>
-      <div className='kasa-accomodation-ratingHost'>
+      <div className="kasa-accomodation-ratingHost">
         <Rating value={accomodation.rating} />
         <Host host={accomodation.host} />
       </div>
-      <div className='kasa-accomodation-collapses'>
-        <Collapse title='Description' content={accomodation.description} />
-        <Collapse title='Équipement' content={accomodation.equipments} />
+      <div className="kasa-accomodation-collapses">
+        <Collapse title="Description" content={accomodation.description} />
+        <Collapse title="Équipement" content={accomodation.equipments} />
       </div>
     </div>
   )
 }
-
